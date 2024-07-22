3c23491918e0afbf96f6205210afdb24636f98c4
Jessica Di Monaco <110666802+jdimonaco@users.noreply.github.com>
2024-07-22 20:58:01 +0100

    Add files via upload

---
+++ Merge-Text-Files/main.py
@@ -0,0 +1,16 @@
+# Import the Path class from the pathlib module for handling filesystem paths
+from pathlib import Path
+
+# Define the directory containing the files to be merged
+files_dir = Path('files')
+
+# # Initialise an empty string to store the merged content
+merged = ''
+for filepath in files_dir.iterdir():
+  with open(filepath, 'r') as file:
+    content = file.read()
+  merged = merged+ content + '\n'
+
+# Open a new file (or overwrite if it exists) named 'merged.csv' in write mode
+with open('merged.csv', 'w') as file:
+  file.write(merged)
