e52b80dad90ba704aed6529e0c1bf3e0af452423
Jessica Di Monaco <110666802+jdimonaco@users.noreply.github.com>
2024-07-25 14:45:38 +0100

    Update main.py

--- Destroy-Files-Forever/main.py
+++ Destroy-Files-Forever/main.py
@@ -1,3 +1,6 @@
+# This code is part of exercises from the Automate Everything with Python by Ardit Sulce.
+# Course URL: https://www.udemy.com/course/automate-everything-with-python/
+
 from pathlib import Path
 
 root_dir = Path('destination')
